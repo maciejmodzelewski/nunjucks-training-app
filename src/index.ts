9f423d9343698e2e85c705ff3ec3f5ace4e59034
Maciej Modzelewski <maciej.modzelewski@gmx.co.uk>
2024-02-10 11:23:13 +0000

    minor chages, mostly done by prettier

--- src/index.ts
+++ src/index.ts
@@ -9,21 +9,22 @@ dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
-app.set("views", [
-  path.join(__dirname, "/views")
-]);
+app.set('views', path.join(__dirname, '/views'));
 
 const nunjuckLoader = new nunjucks.FileSystemLoader(app.get('views'));
 
 const nunjuckLoaderOptions = {
   watch: process.env.NUNJUCKS_LOADER_WATCH === 'true',
-  noCache: process.env.NUNJUCKS_LOADER_NO_CACHE === 'true'
-}
+  noCache: process.env.NUNJUCKS_LOADER_NO_CACHE === 'true',
+};
 
-const nunjucksEnvironment = new nunjucks.Environment(nunjuckLoader, nunjuckLoaderOptions);
+const nunjucksEnvironment = new nunjucks.Environment(
+  nunjuckLoader,
+  nunjuckLoaderOptions
+);
 
 nunjucksEnvironment.express(app);
-app.set("view engine", "njk");
+app.set('view engine', 'njk');
 
 app.get('/', indexController);
 
